0a6398ccd86175d71f2b5f2871824d75fb48d3c2
Evgenii Perminov <perminov.eugene@gmail.com>
2025-02-05 12:56:58 +0200

    Add linters

--- src/dirr_processing.rs
+++ src/dirr_processing.rs
@@ -1,32 +1,30 @@
 use std::fs;
 
-use crate::file_info::FileInfo;
 use crate::file_info::convert_path_meta_to_file_info;
+use crate::file_info::FileInfo;
 
 pub fn fill_up_files_data_by_path(path: &str, files_data: &mut Vec<FileInfo>) {
-  match fs::read_dir(path) {
-      Ok(read_dir_res) => {
-          for entry in read_dir_res {
-              if let Ok(dir) = entry {
-                  let file_meta = match dir.metadata() {
-                      Ok(meta) => meta,
-                      Err(err) => {
-                          println!("Error reading metadata for {:?}: {:?}", dir.path(), err);
-                          continue;
-                      }
-                  };
-                  if file_meta.is_file() {
-                      let file_info = convert_path_meta_to_file_info(&dir.path(), file_meta);
-                      files_data.push(file_info);
-                  } else {
-                      println!("Processing directory: {:?}", dir.path());
-                      fill_up_files_data_by_path(dir.path().to_str().unwrap(), files_data);
-                  }
-              }
-          }
-      }
-      Err(err) => {
-          println!("Error reading directory {:?}: {:?}", path, err);
-      }
-  }
+    match fs::read_dir(path) {
+        Ok(read_dir_res) => {
+            for dir in read_dir_res.flatten() {
+                let file_meta = match dir.metadata() {
+                    Ok(meta) => meta,
+                    Err(err) => {
+                        println!("Error reading metadata for {:?}: {:?}", dir.path(), err);
+                        continue;
+                    }
+                };
+                if file_meta.is_file() {
+                    let file_info = convert_path_meta_to_file_info(&dir.path(), file_meta);
+                    files_data.push(file_info);
+                } else {
+                    println!("Processing directory: {:?}", dir.path());
+                    fill_up_files_data_by_path(dir.path().to_str().unwrap(), files_data);
+                }
+            }
+        }
+        Err(err) => {
+            println!("Error reading directory {:?}: {:?}", path, err);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-
 mod create_source_file;
-mod file_info;
 mod dirr_processing;
+mod file_info;
 
-use dirr_processing::fill_up_files_data_by_path;
 use create_source_file::create_source_file;
+use dirr_processing::fill_up_files_data_by_path;
 
 fn main() {
-  let mut files_data: Vec<file_info::FileInfo> = Vec::new();
-  fill_up_files_data_by_path(".", &mut files_data);
-  create_source_file(&files_data);
+    let mut files_data: Vec<file_info::FileInfo> = Vec::new();
+    fill_up_files_data_by_path(".", &mut files_data);
+    create_source_file(&files_data);
 }
